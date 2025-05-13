163f08630a41dba062836c4dcd6c78cfd16bc336
Fernando <33039864+tuxslack@users.noreply.github.com>
2025-05-13 01:28:20 -0300

    Atualização

--- usr/local/bin/freeze-xfce4-panel.sh
+++ usr/local/bin/freeze-xfce4-panel.sh
@@ -4,7 +4,7 @@
 # Data:        26/02/2016 as 08:59
 #
 # Colaboração: Fernando Souza - https://www.youtube.com/@fernandosuporte/
-# Data:        09/05/2025 as 13:46:13
+# Data:        13/05/2025 as 01:25:45
 # Homepage:    https://github.com/tuxslack/freeze-xfce4-panel
 #
 # Licença padrão GLP v2
@@ -23,8 +23,18 @@
 # que, embora não seja oficial na documentação, pode funcionar como um controle de travamento 
 # se interpretado dessa forma por xfce4-panel.
 # 
+# 
+# O que este script cobre:
+# 
+#     Todos os ficheiros xfce4-panel*.xml dentro da pasta de configuração do XFCE.
+# 
+#     Interface gráfica com yad.
+# 
+#     Reaplica permissões e reinicia o painel após a mudança.
+# 
+#     Usa chattr e chmod em todos os ficheiros encontrados.
 #
-#
+# 
 #
 # This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -62,6 +72,17 @@
 # ChangeLog
 
 
+
+# 12/05/2025  Fernando Souza  <https://github.com/tuxslack>
+
+# Modificação do /etc/xdg/... não tem prioridade:
+
+#  O XFCE carrega primeiro os ficheiros de configuração do utilizador em ~/.config/xfce4/xfconf/..., não os do sistema (/etc/xdg/...).
+
+#  Ou seja, copiar a configuração para /etc/xdg não bloqueia nada se o utilizador tiver sua própria cópia.
+
+
+
 # 09/05/2025  Fernando Souza  <https://github.com/tuxslack>
 
 # Com essas mudanças, o script agora não precisa reiniciar o sistema inteiro - apenas o 
@@ -80,16 +101,52 @@
 # Criação do script
 
 
+
 # ----------------------------------------------------------------------------------------
 
-
 clear
+
+
+# Caminhos:
+
+logotipo="/usr/share/pixmaps/duzeru-freezepanel.png"
+
+
+USUARIO_REAL=$(logname)  # ou use `who | awk '{print $1}' | head -n1`
+
+
+HOME_REAL=$(eval echo "~$USUARIO_REAL")
+
+
+# Diretório de configuração do painel XFCE
+
+PASTA_USUARIO="$HOME_REAL/.config/xfce4/xfconf/xfce-perchannel-xml"
+
+
+# Caminho do arquivo de configuração do painel:
+
+PAINEL_XML="$PASTA_USUARIO/xfce4-panel.xml"
+
+
+
+# Arquivo de log (para auditoria ou debug das ações feitas):
+
+log="/tmp/xfce4-panel.log"
+
+
+
+# echo $PAINEL_XML
+
+# exit 
+
+# ----------------------------------------------------------------------------------------
+
 
 
 # Verificar se os programas estão instalados
 
 
-which yad           1> /dev/null 2> /dev/null || { echo "Programa Yad não esta instalado."      ; exit ; }
+which yad 1> /dev/null 2> /dev/null || { echo "Programa Yad não esta instalado." ; exit ; }
 
 
 
@@ -108,32 +165,26 @@ for cmd in "${REQUIRED_CMDS[@]}"; do
 done
 
 
+# ----------------------------------------------------------------------------------------
+
+clear
+
+
+if [ "$EUID" -ne 0 ]; then
+
+    yad --center --error --title="Permissão Negada" --text="Este script precisa ser executado como Root."
+
+    exit 1
+
+fi
+
 
 # ----------------------------------------------------------------------------------------
 
-# Caminhos:
+# Remove o arquivo de log
 
-logotipo="/usr/share/pixmaps/duzeru-freezepanel.png"
+rm -f "$log" 2>/dev/null
 
-
-# Pasta do usuário: 
-
-PASTA_USUARIO="$HOME/.config/xfce4/xfconf/xfce-perchannel-xml"
-
-
-# Pasta do sistema:
-
-PASTA_SISTEMA="/etc/xdg/xfce4/xfconf/xfce-perchannel-xml"
-
-
-# Arquivo de configuração do painel:
-
-ARQUIVO="xfce4-panel.xml"
-
-
-# Arquivo de log:
-
-log="/tmp/xfce4-panel.log"
 
 # ----------------------------------------------------------------------------------------
 
@@ -150,51 +201,151 @@ fi
 
 # ----------------------------------------------------------------------------------------
 
-# Verificação de arquivos e diretórios:
 
+# Verifica existência da pasta
 
-if [ ! -d "$PASTA_USUARIO" ] || [ ! -f "$PASTA_USUARIO/$ARQUIVO" ]; then
+if [ ! -d "$PASTA_USUARIO" ]; then
 
-    yad --center --window-icon="$logotipo" --error --title="Erro" --text="Arquivo ou pasta do usuário não encontrados: $PASTA_USUARIO/$ARQUIVO" --buttons-layout=center --button=OK:0 2>/dev/null
-
-    exit 1
-fi
-
-
-if [ ! -d "$PASTA_SISTEMA" ]; then
-
-    yad --center --window-icon="$logotipo" --error --title="Erro" --text="Pasta do sistema não encontrada: $PASTA_SISTEMA" --buttons-layout=center --button=OK:0 2>/dev/null
+    yad --center --error --title="Erro" --text="Diretório não encontrado:\n$PASTA_USUARIO"
 
     exit 1
+
 fi
 
 # ----------------------------------------------------------------------------------------
 
-# Remove o arquivo de log
 
-rm -Rf "$log" 2>/dev/null
+# Verifica existência do arquivo
 
+if [ ! -f "$PAINEL_XML" ]; then
 
-clear
+    yad --center --error --title="Erro" --text="Arquivo não encontrado:\n$PAINEL_XML"
 
+    exit 1
 
-# ----------------------------------------------------------------------------------------
-
-echo "
-Versão do painel: `xfce4-panel --version`
-" > "$log"
+fi
 
 
 # ----------------------------------------------------------------------------------------
 
-    # Envia notificação sobre o arquivo de log
+# Função para reiniciar o painel
 
-    notify-send -t 40000 -i edit-paste "Painel do xfce" \
+reiniciar_painel(){
+
+    xfce4-panel --restart   2>> "$log"
+
+    ls -l "$PASTA_USUARIO"  2>> "$log"
+
+}
+
+
+# ----------------------------------------------------------------------------------------
+
+
+# Função para congelar o painel do xfce
+
+
+congelar() {
+
+
+    echo -e "\n❄️ Congelar painel...\n"
+
+# Para nomes com espaços
+
+IFS=$'\n'
+
+    for arq in $arquivos_panel; do
+
+        chmod a-w "$arq"      2>> "$log"
+
+        sudo chattr +i "$arq" 2>> "$log"
+
+    done
+
+unset IFS
+
+
+    echo "Você escolheu travar. Reiniciando o painel XFCE..."
+
+    yad --center --window-icon="$logotipo" --info --title="Painel Congelado" --text="Todos os arquivos de painel foram congelados com sucesso.\n\n\nO painel não poderá ser alterado, nem via interface nem terminal. Até o Root precisa remover o atributo imutável para alterar." --buttons-layout=center --button=OK:0 2>/dev/null
+
+
+   # Para reiniciar apenas o xfce4-panel
+
+   reiniciar_painel
+
+}
+
+
+
+# ----------------------------------------------------------------------------------------
+
+# Função para descongelar o painel do xfce
+
+
+descongelar(){
+
+
+    echo -e "\n🔓 Descongelar painel...\n"
+
+# Para nomes com espaços
+
+IFS=$'\n'
+
+    for arq in $arquivos_panel; do
+
+        sudo chattr -i "$arq" 2>> "$log"
+
+        chmod u+w "$arq"      2>> "$log"
+
+    done
+
+unset IFS
+
+
+    echo "Você escolheu destravar. Reiniciando o painel XFCE..."
+
+    yad --center --window-icon="$logotipo" --info --title="Painel Descongelado" --text="Todos os arquivos de painel foram desbloqueados com sucesso." --buttons-layout=center --button=OK:0 2>/dev/null
+
+
+   # Para reiniciar apenas o xfce4-panel
+
+   reiniciar_painel
+
+}
+
+
+# ----------------------------------------------------------------------------------------
+
+# Procura por todos os arquivos relacionados ao painel
+
+arquivos_panel=$(find "$PASTA_USUARIO" -type f -name "*xfce4-panel*.xml")
+
+if [ -z "$arquivos_panel" ]; then
+
+    yad --center --error --title="Erro" --text="Nenhum arquivo xfce4-panel*.xml encontrado."
+
+    exit 1
+fi
+
+# ----------------------------------------------------------------------------------------
+
+# Log inicial
+
+echo -e "\n\nVersão do painel: $(xfce4-panel --version)\n\n" > "$log"
+
+
+# ----------------------------------------------------------------------------------------
+
+# Notificação
+
+    notify-send -t 40000 -i edit-paste "Painel do XFCE" \
 "
-Ao final do processo verifica o arquivo de log: $log.
+Ao final do processo verifique o arquivo de log: $log.
 "
 
 # ----------------------------------------------------------------------------------------
+
 
 
 ############## APRESENTAÇÃO ###############################
@@ -209,47 +360,52 @@ echo -e '
 ||  \e[34;1m ##   ## ##  ##  ##    ####"   ##     ##  ##  \e[33;1m      \e[32;1m    \\\e[m\e[33;1mo\e[m\e[32;1m/ \e[m    ||
 ||  \e[34;1m ######" ##..## ###..# "#.     ##     ##..##  \e[32;1mSistema   \e[33;1m # \e[m     ||
 ||  \e[34;1m #####%¨ "####"#######  "####  ##     "####"  \e[32;1mGNU\e[m\e[33;1m/\e[m\e[32;1mLinux\e[m\e[32;1m_/ \_ \e[m   ||
-*====================================================================*'
+*====================================================================*
+
+'
 
 
-# Mensagens informativas
+
+# Mensagens de aviso
 
 echo -e "\n\e[32;1mVou auxiliar na segurança do painel, preciso da senha.\e[m"
 echo -e "\e[31;1mATENÇÃO!\e[m"
-echo -e "\e[32;1mApós digitar a senha, o painel será reiniciado.\e[m"
-echo ''
-echo -e "\e[32;1mI will assist in security panel, I need the root password.\e[m"
-echo -e "\e[31;1mWARNING!\e[m"
-echo -e "\e[32;1mAfter entering the password, the panel will reboot. \n\n\e[m"
+echo -e "\e[32;1mApós digitar a senha, o painel será reiniciado.\n\e[m"
 
 
 ############## FIM APRESENTAÇÃO ###############################
 
 
+# Confirmação
 
 yad \
 --center \
 --window-icon="$logotipo" \
 --info \
 --title="Segurança do Painel" \
---text="Vou auxiliar na segurança do painel.\n\n⚠️ ATENÇÃO ⚠️\n\nApós digitar a senha, o painel será reiniciado.\n\n\nI will assist in security panel, I need the root pass.\n\n⚠️ WARNING ⚠️\n\nAfter entering the password, the panel will reboot." \
+--text="Vou auxiliar na segurança do painel.\n\n⚠️ ATENÇÃO ⚠️\n\nApós digitar a senha, o painel será reiniciado." \
 --buttons-layout=center \
 --button=Cancelar:1 --button=OK:0 \
 2>/dev/null
 
 
 
-if [ "$?" == "1" ];
-then 
 
-     clear
+resposta=$?
 
-     exit
-     
+if [ "$resposta" -eq 1 ]; then
+
+    clear
+
+    exit
 fi
+
 
 ############## Escolha da opção ###################
  
+
+# Mostrar menu de opções
+
 
 # resposta=$(yad --center --window-icon="$logotipo" --entry --title="Painel XFCE" --text="Escolha uma opção:\n\nd - Descongelar painel (Unfreeze)\nc - Congelar painel (Freeze)" --entry-text="d" "c")
 
@@ -264,20 +420,19 @@ yad --center \
     --width="450" --height="150" \
      2>/dev/null
 
+
 resposta=$?
 
+if [ "$resposta" -eq 1 ]; then
 
-if [ "$?" == "1" ];
-then 
+    clear
 
-     clear
-
-     exit
-     
+    exit
 fi
 
 
-############## Lógica de Escolha ###############################
+
+############## Ações ###############################
 
 if [ "$resposta" -eq 0 ]; then
 
@@ -288,46 +443,12 @@ if [ "$resposta" -eq 0 ]; then
     # Isso provavelmente força o painel a ficar bloqueado contra alterações de usuários comuns.
 
 
-    echo -e "\nCongelar...\n"
+    # Para congelar o painel do xfce
+
+    congelar
 
 
-    # Copia o arquivo do usuário para o diretório do sistema (/etc/xdg/...).
-
-    sudo cp -a "$PASTA_USUARIO/$ARQUIVO" "$PASTA_SISTEMA/" 2>> "$log"
-
-
-    # Altera a tag <channel name="xfce4-panel" version="1.0"> para incluir o atributo unlocked="root".
-
-    sudo sed -i 's/<channel name="xfce4-panel" version="1.0">/<channel name="xfce4-panel" version="1.0" unlocked="root">/g' "$PASTA_SISTEMA/$ARQUIVO" 2>> "$log"
-
-
-    # Exibe uma mensagem com o yad e reinicia o sistema para aplicar a configuração.
-
-    # yad --center --window-icon="$logotipo" --info --title="Congelar Painel" --text="Você escolheu travar.\nO sistema será reiniciado para aplicar as configurações." --button=OK:0 2>/dev/null
-
-
-    # echo "Você escolheu travar, aguarde vou reiniciar para 
-# finalizar as configurações até logo..."
-
-#    sleep 5
-
-
-    # sudo shutdown -r now
-
-
-
-    yad --center --window-icon="$logotipo" --info --title="Congelar Painel" --text="Você escolheu travar.\nO painel será reiniciado agora para aplicar as configurações." --buttons-layout=center --button=OK:0 2>/dev/null
-
-    echo "Você escolheu travar. Reiniciando o painel XFCE..."
-
-
-    # Para reiniciar apenas o xfce4-panel
-
-    xfce4-panel --restart
-
-
-    ls -l "$PASTA_SISTEMA"
-
+    echo "O painel foi congelado com sucesso.\n\nImpossível editar até desbloquear."
 
 
 elif [ "$resposta" -eq 2 ]; then
@@ -336,44 +457,18 @@ elif [ "$resposta" -eq 2 ]; then
     # 🔓 Descongelar (Unfreeze):
 
 
-    echo -e "\nDescongelar...\n"
+    # Para descongelar o painel do xfce
 
-    # Repete a cópia do arquivo do usuário para o sistema.
-
-    sudo cp -a "$PASTA_USUARIO/$ARQUIVO" "$PASTA_SISTEMA/"  2>> "$log"
+    descongelar
 
 
-    # Remove o atributo unlocked="root" do arquivo.
+    echo "O painel foi descongelado.\n\nAgora pode ser editado normalmente."
 
-    sudo sed -i 's/<channel name="xfce4-panel" version="1.0" unlocked="root">/<channel name="xfce4-panel" version="1.0">/g' "$PASTA_SISTEMA/$ARQUIVO" 2>> "$log"
+else
 
+    # Cancelado ou outro código
 
-    # Exibe uma mensagem e reinicia novamente o sistema para aplicar.
-
-    # yad --center --window-icon="$logotipo" --info --title="Descongelar Painel" --text="Você escolheu destravar.\nO sistema será reiniciado para aplicar as configurações." --buttons-layout=center --button=OK:0 2>/dev/null
-
-    # echo "Você escolheu destravar, aguarde vou reiniciar para 
-# finalizar as configurações até logo..."
-
-#    sleep 5
-
-
-    # sudo shutdown -r now
-
-
-
-    yad --center --window-icon="$logotipo" --info --title="Descongelar Painel" --text="Você escolheu destravar.\nO painel será reiniciado agora para aplicar as configurações." --buttons-layout=center --button=OK:0 2>/dev/null
-
-    echo "Você escolheu destravar. Reiniciando o painel XFCE..."
-
-
-    # Para reiniciar apenas o xfce4-panel
-
-    xfce4-panel --restart
-
-
-    ls -l "$PASTA_SISTEMA"
-
+    exit 1
 
 fi
 
